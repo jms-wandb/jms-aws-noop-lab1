22abdfb1416ddb709e21a4398fb9f60b7a71f99d
Jonathan Stanley <jonathan.stanley@wandb.com>
2024-08-01 12:18:16 -0400

    working configs

---
+++ variables.tf
@@ -0,0 +1,86 @@
+
+variable "namespace" {
+  type        = string
+  description = "Name prefix used for resources"
+}
+
+variable "domain_name" {
+  type        = string
+  description = "Domain name used to access instance."
+}
+
+variable "zone_id" {
+  type        = string
+  description = "Id of Route53 zone"
+}
+
+variable "subdomain" {
+  type        = string
+  default     = null
+  description = "Subdomain for accessing the Weights & Biases UI."
+}
+
+variable "wandb_license" {
+  type = string
+}
+
+variable "database_engine_version" {
+  description = "Version for MySQL Auora"
+  type        = string
+  default     = "8.0.mysql_aurora.3.05.2"
+}
+
+variable "database_instance_class" {
+  description = "Instance type to use by database master instance."
+  type        = string
+  default     = "db.r5.large"
+}
+
+variable "database_snapshot_identifier" {
+  description = "Specifies whether or not to create this cluster from a snapshot. You can use either the name or ARN when specifying a DB cluster snapshot, or the ARN when specifying a DB snapshot"
+  type        = string
+  default     = null
+}
+
+variable "database_sort_buffer_size" {
+  description = "Specifies the sort_buffer_size value to set for the database"
+  type        = number
+  default     = 262144
+}
+
+variable "wandb_version" {
+  description = "The version of Weights & Biases local to deploy."
+  type        = string
+  default     = "latest"
+}
+
+variable "wandb_image" {
+  description = "Docker repository of to pull the wandb image from."
+  type        = string
+  default     = "wandb/local"
+}
+
+variable "bucket_name" {
+  type    = string
+  default = ""
+}
+
+variable "bucket_kms_key_arn" {
+  type        = string
+  description = "The Amazon Resource Name of the KMS key with which S3 storage bucket objects will be encrypted."
+  default     = ""
+}
+
+
+variable "allowed_inbound_cidr" {
+  default  = ["0.0.0.0/0"]
+  nullable = false
+  type     = list(string)
+}
+
+
+variable "allowed_inbound_ipv6_cidr" {
+  default  = ["::/0"]
+  nullable = false
+  type     = list(string)
+}
